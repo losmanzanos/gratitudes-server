f208e18da8440ecd1c03408ccd4ecc5a688aa774
Chad Moravec <moravec@colorado.edu>
2021-01-30 09:36:45 -0700

    Production build

--- migrations/002.do.create_gratitude_users.sql
+++ migrations/002.do.create_gratitude_users.sql
@@ -3,7 +3,6 @@ CREATE TABLE gratitude_users (
   user_name TEXT NOT NULL UNIQUE,
   full_name TEXT NOT NULL,
   password TEXT NOT NULL,
-  nickname TEXT,
   date_created TIMESTAMP DEFAULT now() NOT NULL,
   date_modified TIMESTAMP
 );
--- src/config.js
+++ src/config.js
@@ -3,6 +3,9 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   DATABASE_URL:
     process.env.DATABASE_URL || "postgresql://postgres@localhost/gratitude",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://postgres@localhost/noteful-test",
   JWT_SECRET: process.env.JWT_SECRET || "change-this-secret",
   JWT_EXPIRY: process.env.JWT_EXPIRY || "3h",
 };
